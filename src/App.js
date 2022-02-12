e5bba81535d3e3573d668064cc43d669d2d1d615
azadsarxanli <azad.sarxanli@gmail.com>
2022-02-12 18:51:08 +0400

    react todo

--- src/App.js
+++ src/App.js
@@ -1,23 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
 
 function App() {
+  const [user, setUser] = useState("");
+  const [error, setError] = useState(null);
+  const [isUserActive, setIsUserActive] = useState(false);
+  const onChangeHandler = (event) => {
+    setUser(event.target.value);
+  };
+  const onClickHandler = () => {
+    if (user.toLowerCase().charAt(0) !== "a") {
+      setError('İstifadəçi adınız "a" hərfi ilə başlamalıdır.');
+    } else {
+      setIsUserActive(true);
+      setError(null);
+    }
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      {!isUserActive && (
+        <>
+          <input
+            onChange={onChangeHandler}
+            value={user}
+            placeholder="Enter your name..."
+          />
+          {error && <div className="error">{error}</div>}
+          <button onClick={onClickHandler}>Login</button>
+        </>
+      )}
+
+      {isUserActive && (
+        <>
+          <p>Xoş gəldiniz, {user}!</p>
+          <button onClick={() => setIsUserActive(false)}> Log out!</button>
+        </>
+      )}
     </div>
   );
 }
